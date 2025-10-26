41599edacfbefb7b9b2516e41b558bfef2aefe87
Swaraj Darekar <swarajdarekar10@gmail.com>
2025-10-27 00:58:24 +0530

    Update app.js 2(added render's endpoints

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid'); // Synchronous import
 const logger = require('./config/logger');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
@@ -12,18 +11,50 @@ const corsMiddleware = require('./middleware/cors');
 
 const app = express();
 
+
+
 app.use(helmet());
+
 app.use(corsMiddleware);
+
 app.use(express.json({ limit: '10mb' }));
+
 app.use(express.urlencoded({ extended: true }));
 
+
+
 app.use((req, res, next) => {
+
   req.id = uuidv4();
+
   logger.info(`Request received: ${req.method} ${req.path}`,
+
    {
+
      requestId: req.id 
+
     });
+
   next();
+
+});
+
+
+
+// Render health check routes
+
+app.get('/', (req, res) => {
+
+  res.send('Backend is running ✅');
+
+});
+
+
+
+app.head('/', (req, res) => {
+
+  res.sendStatus(200);
+
 });
 
 
